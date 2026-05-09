31d260494b0d488ccd6920e1f77e7a1edbd5ad69
ronitchauhan20 <ronit25csu204@ncuindia.edu>
2026-05-09 11:56:34 +0530

    Create function_array.cpp

---
+++ function_array.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+
+// void computePerformance(int m[], int n){
+//     int max=m[0], min=m[0], avg=m[0];
+
+//     for(int i=1 ; i<n ; i++){
+//         if(max<m[i]){
+//             max=m[i];
+//         }
+//         if(min>m[i]){
+//             min=m[i];
+//         }
+//         avg=avg+m[i];
+//     }
+//     cout<<max<<"\n"<<min<<"\n"<<float(avg)/n;
+// }
+
+// int main(){
+//     int marks[]={38,38,8,398,324,294,674,63,78,12};
+//     computePerformance(marks,10);
+//     return 0;
+// }
